2c87cfdb8323fc98c9e1e052ee222bc0c6054f90
newbooks <junjun.mao@gmail.com>
2020-12-28 14:48:59 -0500

    completed, solve one problem with arguments, and all without argements

--- 24solver.py
+++ 24solver.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
 import sys
-
+from itertools import combinations
+from itertools import combinations_with_replacement
 
 class Number:
     def __init__(self):
@@ -9,10 +10,11 @@ class Number:
         self.history = ""
 
     def printme(self):
-        print("My value is %d, and my history is %s" % (self.value, self.history))
+        print("%d = %s" % (self.value, self.history))
 
 
-def tryout2(two_numbers):
+def reduce(two_numbers):
+    "Reduce two numbers to one"
     results = []
     a = two_numbers[0]
     b = two_numbers[1]
@@ -42,16 +44,18 @@ def tryout2(two_numbers):
     results.append(c)
 
     # a / b
-    c = Number()
-    c.value = a.value / b.value
-    c.history = "(" + a.history + "/" + b.history + ")"
-    results.append(c)
+    if b.value != 0:
+        c = Number()
+        c.value = a.value / b.value
+        c.history = "(" + a.history + "/" + b.history + ")"
+        results.append(c)
 
     # b / a
-    c = Number()
-    c.value = b.value / a.value
-    c.history = "(" + b.history + "/" + a.history + ")"
-    results.append(c)
+    if a.value != 0:
+        c = Number()
+        c.value = b.value / a.value
+        c.history = "(" + b.history + "/" + a.history + ")"
+        results.append(c)
 
     # Throw away negative values
     results = [x for x in results if x.value >= 0.0]
@@ -67,49 +71,73 @@ def tryout2(two_numbers):
     return new_results
 
 
-def tryout3(three_numbers):
-    results = []
+def solver(numbers):
+    queue = [numbers]
+    singles = []
+    while queue:
+        #print("Begining of loop, length of Q = %d" % len(queue))
 
-    for i_exclude in range(3):
-        two_numbers = []
-        for j in range(3):
-            if j != i_exclude:
-                two_numbers.append(three_numbers[j])
+        # pop a numbers object
+        object = queue.pop(0)
 
-        step1_results = tryout2(two_numbers)
-        for x in step1_results:
-            two_numbers = [x, three_numbers[i_exclude]]
-            step2_results = tryout2(two_numbers)
-            results = results + step2_results
+        # pick two numbers from the object
+        picked_list = combinations(object, 2)
 
-    new_results = []
-    appeared_values = []
-    for x in results:
-        if x.value not in appeared_values:
-            new_results.append(x)
-            appeared_values.append(x.value)
+        #print("Length of object: %d" % len(object))
+        for picked in picked_list:
+            the_rest = set(object) - set(picked)
 
-    return new_results
+            # reduce two numbers to one,
+            reduced_list = reduce(picked)
+            #print("Remaining=", len(the_rest))
+            if len(the_rest) > 0:  # if the object has other member, put them together and append to the queue
+                for reduced in reduced_list:
+                    #print({reduced} | the_rest)
+                    queue.append(list(the_rest | {reduced}))
+                    #print(len(queue))
+            else:   # else it is a single object now, put to singles
+                for reduced in reduced_list:
+                    singles.append(reduced)
 
+        #print("End of loop, length of Q = %d" % len(queue))
+
+    for x in singles:
+        if x.value == 24:
+            return x
+
+    return None
 
 if __name__ == "__main__":
 
-    numbers = []
-    for x in sys.argv[1:]:
-        n = Number()
-        n.value = int(x)
-        n.history = x
-        numbers.append(n)
-        # n.printme()
+    if len(sys.argv) > 2:
+        numbers = []
+        for x in sys.argv[1:]:
+            n = Number()
+            n.value = int(x)
+            n.history = x
+            numbers.append(n)
 
-    # We have 3 numbers
-    outcomes = tryout3(numbers[:3])
-    for outcome in outcomes:
-        print(outcome.value, "=", outcome.history)
+        solution = solver(numbers)
+        if solution:
+            solution.printme()
+    else:  # investigate all
+        all_numbers = combinations_with_replacement([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 4)
+        all_numbers = list(all_numbers)
+        N_allcombos = len(all_numbers)
+        Counter = 0
+        for numbers_int in all_numbers:
+            numbers = []
+            for x in numbers_int:
+                n = Number()
+                n.value = x
+                n.history = str(x)
+                numbers.append(n)
+            solution = solver(numbers)
+            if solution:
+                print("%s: " % str(numbers_int), end="")
+                solution.printme()
+                Counter += 1
+
+        print("%d solutions out of %d combinations. Solvable rate = %.2f%%" % (Counter, N_allcombos, Counter/N_allcombos*100))
 
 
-    # We have two numbers
-    # outcomes = tryout2(numbers[:2])
-    #
-    # for outcome in outcomes:
-    #     print(outcome.value, "=", outcome.history)
